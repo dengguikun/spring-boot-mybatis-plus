362282bc27c9cc99c0561f7a86320bcd5ab611a6
dengguikun <347319299@qq.com>
2018-12-27 09:34:35 +0800

    添加过滤器

--- src/main/java/com/gaken/first/FirstApplication.java
+++ src/main/java/com/gaken/first/FirstApplication.java
@@ -3,14 +3,17 @@ package com.gaken.first;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description: 应用启动类
  * @author: dengguikun
  * @date: 2018-12-26
+ *
+ * SpringBootApplication 上使用@ServletComponentScan注解后，Servlet、Filter、Listener 可以直接通过 @WebServlet、
  */
-
+@ServletComponentScan
 @MapperScan(value = "com.gaken.first.mapper")
 @SpringBootApplication
 public class FirstApplication {
